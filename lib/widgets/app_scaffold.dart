6ad3d348633341bdac5011b2ff40ff3bb3bf0c09
Ethan Harstad <ethanharstad@gmail.com>
2021-12-27 15:17:52 -0600

    Add actions to AppScaffold

--- lib/widgets/app_scaffold.dart
+++ lib/widgets/app_scaffold.dart
@@ -6,16 +6,19 @@ class AppScaffold extends StatelessWidget {
     Key? key,
     required this.title,
     required this.body,
+    this.actions = const [],
   }) : super(key: key);
 
   final String title;
   final Widget body;
+  final List<Widget> actions;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        actions: actions,
       ),
       drawer: const AppDrawer(),
       body: body,
