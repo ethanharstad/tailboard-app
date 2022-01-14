c6e767d267e81c4323b57749f33477d10d734e69
Ethan Harstad <ethanharstad@gmail.com>
2022-01-14 16:19:02 -0600

    Switch protocol screen to list instead of tiles

--- lib/screens/quick_protocols_screen.dart
+++ lib/screens/quick_protocols_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tailboard_app/screens/bls_protocol_screen.dart';
 import 'package:tailboard_app/widgets/app_scaffold.dart';
-import 'package:tailboard_app/widgets/launcher_tile.dart';
 import 'package:tailboard_app/widgets/unimplemented_dialog.dart';
 
 class QuickProtocolsScreen extends StatelessWidget {
@@ -9,56 +8,106 @@ class QuickProtocolsScreen extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  Widget buildItem({
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return Card(
+      child: ListTile(
+        onTap: onTap,
+        title: Text(title),
+        leading: Icon(icon, size: 48),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
       title: 'Quick Protocols',
-      body: GridView.count(
-        padding: const EdgeInsets.all(16),
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        crossAxisCount: 2,
-        children: <Widget>[
-          LauncherTile(
-            icon: Icons.local_hospital,
-            title: 'BLS',
-            onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                builder: (BuildContext context) => const BLSProtocolScreen())),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Wrap(
+              alignment: WrapAlignment.start,
+              spacing: 8.0,
+              runSpacing: 4.0,
+              children: <Widget>[
+                FilterChip(
+                  label: const Text('BLS'),
+                  onSelected: (bool selected) {},
+                ),
+                FilterChip(
+                  label: const Text('ALS'),
+                  onSelected: (bool selected) {},
+                ),
+                FilterChip(
+                  label: const Text('Adult'),
+                  onSelected: (bool selected) {},
+                ),
+                FilterChip(
+                  label: const Text('Pediatric'),
+                  onSelected: (bool selected) {},
+                ),
+              ],
+            ),
           ),
-          LauncherTile(
-            icon: Icons.favorite,
-            title: 'ACLS',
-            onTap: () => showDialog<void>(
-                context: context,
-                builder: (BuildContext context) => const UnimplementedDialog()),
-          ),
-          LauncherTile(
-            icon: Icons.remove_circle,
-            title: 'Bradycardia',
-            onTap: () => showDialog<void>(
-                context: context,
-                builder: (BuildContext context) => const UnimplementedDialog()),
-          ),
-          LauncherTile(
-            icon: Icons.add_circle,
-            title: 'Tachycardia',
-            onTap: () => showDialog<void>(
-                context: context,
-                builder: (BuildContext context) => const UnimplementedDialog()),
-          ),
-          LauncherTile(
-            icon: Icons.medical_services,
-            title: 'ROSC',
-            onTap: () => showDialog<void>(
-                context: context,
-                builder: (BuildContext context) => const UnimplementedDialog()),
-          ),
-          LauncherTile(
-            icon: Icons.local_pharmacy,
-            title: 'Opioids',
-            onTap: () => showDialog<void>(
-                context: context,
-                builder: (BuildContext context) => const UnimplementedDialog()),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(4),
+              children: <Widget>[
+                buildItem(
+                  icon: Icons.local_hospital,
+                  title: 'BLS',
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          const BLSProtocolScreen())),
+                ),
+                buildItem(
+                  icon: Icons.favorite,
+                  title: 'ACLS',
+                  onTap: () => showDialog<void>(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          const UnimplementedDialog()),
+                ),
+                buildItem(
+                  icon: Icons.remove_circle,
+                  title: 'Bradycardia',
+                  onTap: () => showDialog<void>(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          const UnimplementedDialog()),
+                ),
+                buildItem(
+                  icon: Icons.add_circle,
+                  title: 'Tachycardia',
+                  onTap: () => showDialog<void>(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          const UnimplementedDialog()),
+                ),
+                buildItem(
+                  icon: Icons.medical_services,
+                  title: 'ROSC',
+                  onTap: () => showDialog<void>(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          const UnimplementedDialog()),
+                ),
+                buildItem(
+                  icon: Icons.local_pharmacy,
+                  title: 'Opioids',
+                  onTap: () => showDialog<void>(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          const UnimplementedDialog()),
+                ),
+              ],
+            ),
           ),
         ],
       ),
