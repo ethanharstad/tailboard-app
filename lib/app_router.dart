ee854585c5ea5a7463cb2c852637d0138b28f4ce
Ethan Harstad <ethanharstad@gmail.com>
2024-06-18 15:10:32 -0500

    Nest app routes under home route

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -4,11 +4,8 @@ import 'package:go_router/go_router.dart';
 import 'package:tailboard_app/meds/screens/med_calc_screen.dart';
 import 'package:tailboard_app/meds/screens/med_list_screen.dart';
 import 'package:tailboard_app/protocols/screens/algorithm_list_screen.dart';
-import 'package:tailboard_app/repositories/user_repository.dart';
 import 'package:tailboard_app/screens/home_screen.dart';
 import 'package:tailboard_app/screens/login_screen.dart';
-
-final UserRepository _userRepository = UserRepository();
 
 final GoRouter router = GoRouter(
   redirect: (BuildContext context, GoRouterState state) {
@@ -23,29 +20,31 @@ final GoRouter router = GoRouter(
       name: "home",
       builder: (BuildContext context, GoRouterState state) =>
           const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: "protocols",
+          name: "protocols",
+          builder: (BuildContext context, GoRouterState state) =>
+          const AlgorithmListScreen(),
+        ),
+        GoRoute(
+          path: "medications",
+          name: "medications",
+          builder: (BuildContext context, GoRouterState state) =>
+          const MedListScreen(),
+        ),
+        GoRoute(
+          path: "medicationCalculator",
+          name: "medication_calculator",
+          builder: (BuildContext context, GoRouterState state) =>
+          const MedCalcScreen(),
+        ),
+      ]
     ),
     GoRoute(
       path: '/login',
       name: 'login',
       builder: (BuildContext context, GoRouterState state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: "/protocols",
-      name: "protocols",
-      builder: (BuildContext context, GoRouterState state) =>
-          const AlgorithmListScreen(),
-    ),
-    GoRoute(
-      path: "/medications",
-      name: "medications",
-      builder: (BuildContext context, GoRouterState state) =>
-          const MedListScreen(),
-    ),
-    GoRoute(
-      path: "/medicationCalculator",
-      name: "medication_calculator",
-      builder: (BuildContext context, GoRouterState state) =>
-          const MedCalcScreen(),
     ),
   ],
 );
